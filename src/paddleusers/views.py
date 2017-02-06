5cbee09746d0e3a427a445354310213fd003b5fa
tommp <tompersen@gmail.com>
2017-02-06 22:33:55 +0100

    Added prod logo

--- src/paddleusers/views.py
+++ src/paddleusers/views.py
@@ -5,11 +5,12 @@ from datetime import datetime, timedelta
 
 from paddleusers.models import Position
 
-class BoardView(ListView):
-	template_name = 'board_page.html'
-	queryset=Position.objects.all()
 
-	def get_context_data(self, **kwargs):
-		context = super(BoardView, self).get_context_data(**kwargs)
-		context['board_positions'] = Position.objects.all().order_by('priority')
-		return context
+class BoardView(ListView):
+    template_name = 'board_page.html'
+    queryset = Position.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(BoardView, self).get_context_data(**kwargs)
+        context['board_positions'] = Position.objects.all().order_by('priority')
+        return context
